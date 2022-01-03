9df87174da77c17a6bc3687221a7cd2ebcdc6a5b
Josh Gulledge <joshcalebgulledge@gmail.com>
2022-01-02 23:19:18 -0600

    starting first challenge

---
+++ README.md
@@ -0,0 +1,4 @@
+# Personal Daily challenge
+The goal of this is to do a personal code challenge every day of the week
+
+I will be getting this challenges from the codewar or leetcode sites
---
+++ daily.js
@@ -0,0 +1,20 @@
+/*
+From: CodeWar
+Red Knight is chasing two pawns. Which pawn will be caught, and where?
+
+Input / Output
+Input will be two integers:
+
+N / n (Ruby) vertical position of Red Knight (0 or 1).
+P / p (Ruby) horizontal position of two pawns (between 2 and 1000000).
+Output has to be a tuple (python, haskell, Rust, prolog, C#), an array (javascript, ruby), an object (java), or a structure (C) with:
+
+"Black" or "White" - which pawn was caught
+Where it was caught (horizontal position)
+*/
+
+function redKnight(N, P) {
+  return "hello world";
+};
+
+console.log(redKnight(0, 8)); // should return ['White', 16]
