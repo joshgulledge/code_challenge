fdf060a83ea6fd7af3a3305474691c4c153ec525
Josh Gulledge <joshcalebgulledge@gmail.com>
2022-01-17 22:47:47 -0600

    set up for working on solution for SumOfDigits in c#

--- csharp/Main.cs
+++ csharp/Main.cs
@@ -6,15 +6,11 @@ namespace DailyTraining
     {
       static void Main(string[] args)
       {
+        int testOne = SumOfDigits.DigitalRoot(16);
+        Console.WriteLine(testOne); // should return 7
 
-        string testOne = HighestLowest.HighAndLow("1 2 3 4 5");
-        Console.WriteLine(testOne); // should return "5 1"
-
-        string testTwo = HighestLowest.HighAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4");
-        Console.WriteLine(testTwo); // return "42 -9"
-
-        string testThree = HighestLowest.HighAndLow("1 9 3 4 -5");
-        Console.WriteLine(testThree); // should return "9 -5"
+        int testTwo = SumOfDigits.DigitalRoot(942);
+        Console.WriteLine(testTwo); // should return 6
       }      
 
     }
---
+++ csharp/SumOfDigits.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace DailyTraining
+{
+  public class SumOfDigits
+  {
+    public static int DigitalRoot(long n)
+    {
+      // Your awesome code here!
+      return 7;
+    }
+  }
+}
